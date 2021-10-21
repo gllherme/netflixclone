495e313d9f23bb03ea055369d5d7106d907cf017
gllherme <guicampos159@gmail.com>
2021-10-21 14:14:52 -0300

    featured movie details

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,6 +2,13 @@ import React from "react";
 import './FeaturedMovie.css';
 
 export default ({item}) => {
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres) {
+        genres.push(item.genres[i].name);
+    }
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -11,6 +18,18 @@ export default ({item}) => {
             <div className="featured--vertical">
                 <div className="featured--horizontal">   
                     <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
+                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons != 1 ? 's' : ' '}</div>
+                    </div>
+                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--buttons">
+                        <a href="">► Assistir</a>
+                        <a href="">+ Minha lista</a>
+
+                    </div>
+                    <div className="featured--genres"><strong>Gêneros: </strong>{genres.join(', ')}</div>
                 </div> 
             </div>
         </section>
